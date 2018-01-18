636ac261178e12cc6a9a08a68c098d7fc2fc1469
Filip <filip.kisic@live.com>
2018-01-18 15:45:01 +0100

    added projects page

--- server.js
+++ server.js
@@ -52,6 +52,13 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page'
+    });
+});
+
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'requested page can not be loaded'
